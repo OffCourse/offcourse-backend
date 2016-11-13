dc651fb303c5b1ca84a72770dda049b9386cb262
Jan Hein Hoogstad <yeehaa@offcourse.io>
2016-11-13 23:49:31 +0100

    minor chances

--- functions/src/app/retrieve/specs.cljs
+++ functions/src/app/retrieve/specs.cljs
@@ -2,7 +2,8 @@
   (:require [shared.specs.action :as action :refer [action-spec]]
             [cljs.spec :as spec]
             [shared.specs.raw :as raw]
-            [shared.specs.github :as github]))
+            [shared.specs.github :as github]
+            [shared.specs.embedly :as embedly]))
 
 
 (spec/def ::action-types action/types)
@@ -10,6 +11,7 @@
 (defn actions []
   (defmethod action-spec :put [_]
     (spec/tuple ::action-types (spec/or :courses (spec/coll-of ::raw/course)
+                                        :raw-resources (spec/coll-of ::embedly/resource)
                                         :github-courses (spec/coll-of ::github/course)
                                         :github-repos (spec/coll-of ::github/repo)
                                         :raw-users (spec/coll-of ::raw/user)))))
